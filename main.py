709ea0b658a5c64668740b28f2a2dc673d58c519
Raphael Adamski <raphaeladamski@hotmail.com>
2018-04-22 12:13:32 -0300

    moments of Hu

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
-import cv2
 from os import listdir, path, makedirs
 
 import numpy as np
 from skimage.restoration import denoise_wavelet
-from sklearn import linear_model, metrics, preprocessing, model_selection
-from scipy.stats import moment
+from skimage import io, measure, feature; io.use_plugin('matplotlib')
+from sklearn import linear_model, metrics, model_selection
+from scipy import ndimage
 
 from joblib import Parallel, delayed
 import multiprocessing
 
 
+def rescale(X, min=0., max=255., min_original=-1, max_original=-1):
+    """
+        X numpy array like matrix
+        if min_original or  max_original are not explict given,
+        this funcition tries to use X.min() and X.max() to fit values
+        """
+    if min_original == -1 or max_original == -1:
+        min_original = X.min()
+        max_original = X.max()
+
+    return ((max - min) / (max_original - min_original)) * (X - min_original) + min
+
+
 def feature_extraction(img_path):
     print(img_path)
-    img = cv2.imread(img_path)
-    deionised = denoise_wavelet(img, multichannel=True)
-    finger_print = img * (deionised - img) / img**2
-    return [moment(finger_print[:, :, 0], moment=1),
-            moment(finger_print[:, :, 0], moment=2),
-            moment(finger_print[:, :, 0], moment=3),
-            moment(finger_print[:, :, 1], moment=1),
-            moment(finger_print[:, :, 1], moment=2),
-            moment(finger_print[:, :, 1], moment=3),
-            moment(finger_print[:, :, 2], moment=1),
-            moment(finger_print[:, :, 2], moment=2),
-            moment(finger_print[:, :, 2], moment=3)]
+    img = io.imread(img_path)
+    deionised = denoise_wavelet(img, multichannel=True, )
+    finger_print = img.astype('float') * (img.astype('float') - deionised.astype('float')) / (img.astype('float')**2)
+    finger_print = np.nan_to_num(finger_print, copy=False)
+    #finger_print = rescale(finger_print, 0., 1.)
+    features = np.zeros((7*3))
+    for i in range(3):
+        m = measure.moments(finger_print[:, :, i], order=5)
+        cr = m[0, 1] / m[0, 0]
+        cc = m[1, 0] / m[0, 0]
+        mc = measure.moments_central(finger_print[:, :, i], cr, cc, order=5)
+        mn = measure.moments_normalized(mc)
+        mu = measure.moments_hu(mn)
+        features[i*7:i*7+7] = mu
 
 
 dirName = 'train'
@@ -39,7 +54,7 @@ for clazz in classes:
     classPaths = []
     print("Creating paths on class " + str(classesDic[clazz]))
     for image in listdir(dirName + '/' + clazz):
-        if len(classPaths)>=50: break
+        if len(classPaths)>=20: break
         classPaths.append(dirName + '/' + clazz + '/' + image)
         trainClasses.append(classesDic[clazz])
     completeFilesPath.append(classPaths)
@@ -50,7 +65,7 @@ for clazz in classes:
     if path.isfile(fileName):
         clazzSamples = np.load(fileName)
     else:
-        clazzSamples = Parallel(n_jobs=num_cores)(
+        clazzSamples = Parallel(n_jobs=2)(
             delayed(feature_extraction)(i) for i in completeFilesPath[classesDic[clazz]])
         clazzSamples = np.array(clazzSamples)
         np.save(fileName, clazzSamples)
@@ -61,7 +76,6 @@ for clazz in classes:
         trainSamples = np.append(trainSamples, clazzSamples, axis=0)
 
 # independently normalize each sample
-trainSamples = preprocessing.normalize(np.array(trainSamples), axis=1)
 trainClasses = np.array(trainClasses)
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(trainSamples, trainClasses, train_size=0.8)
